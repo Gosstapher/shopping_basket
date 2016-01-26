71fa8c9950d37893ddaa8d4ecbdc2c88183a35e9
Gosstapher <gosstapher@gmail.com>
2016-01-26 15:27:57 +0000

    2nd test ok so far

--- basket.js
+++ basket.js
@@ -1,5 +1,9 @@
 var basket = {
-  totalValue: 0
+  totalBaseValue: 0,
+  contents: [],
+  addItem: function(item){
+    contents.push(item);
+  } 
 }
 
 module.exports = basket;
--- test.js
+++ test.js
@@ -4,8 +4,12 @@ var items = require('./items');
 
 
 describe("Shopping Basket", function(){
-// 1. An empty shopping basket should have a total value of 0
+  // 1. An empty shopping basket should have a total value of 0
   it("An empty shopping basket should have a total value of 0", function(){
-    assert.equal(0,basket.totalValue)
+    assert.equal(0, basket.totalBaseValue)
+  });
+  // 2. An empty shopping basket should have 0 contents 
+  it("An empty shopping basket should have 0 contents", function(){
+    assert.equal(0, basket.contents.length)
   });
 });
