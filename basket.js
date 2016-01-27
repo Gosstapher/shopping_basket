cfe9fee20c1b55983bc48e455c89d42852c02feb
Gosstapher <gosstapher@gmail.com>
2016-01-27 08:33:39 +0000

    new way of adding multiple items work so far

--- basket.js
+++ basket.js
@@ -2,20 +2,14 @@ var items = require('./items')
 
 var basket = {
   totalBaseValue: 0,
-  totalReductionValue:0,
+  // totalReductionValue:0,
   totalFinalValue: 0,
   contents: [],
-  bogofArray: [],
-  addItem: function(item){
+  // bogofArray: [],
+  addItem: function(item, amount){
     this.contents.push(item);
-    this.totalBaseValue += item.price;
-  },
-  calculateBogofReduction: function(){
-    for(item in this.contents){
-      if(item.bogof === true){
-        this.bogofArray.push(item)
-      }
-    }
+    this.totalBaseValue += (item.price * amount);
+    item.amount = amount;
   },
   calculateTotalFinalValue: function(){
     if(this.totalBaseValue <= 19.99){
@@ -28,17 +22,14 @@ var basket = {
   } 
 
 }
-// basket.addItem(items[0]);
-// basket.addItem(items[0]);
-// basket.addItem(items[1]);
-basket.addItem(items[2]);
-basket.addItem(items[2]);
-basket.addItem(items[2]);
-// basket.addItem(items[3]);
-basket.calculateBogofReduction();
+basket.addItem(items[0], 2);
+basket.addItem(items[1], 1);
+basket.addItem(items[2], 3);
+basket.addItem(items[3], 5);
+// basket.calculateBogofReduction();
 console.log(basket.contents);
-console.log(basket.bogofArray);
-// console.log(basket.totalBaseValue);
-// basket.calculateTotalFinalValue();
-// console.log(basket.totalFinalValue);
+// console.log(basket.bogofArray);
+console.log(basket.totalBaseValue);
+basket.calculateTotalFinalValue();
+console.log(basket.totalFinalValue);
 module.exports = basket;
--- items.js
+++ items.js
@@ -1,25 +1,29 @@
 var firstItem = {
   name: "bread",
   price: 0.50,
-  bogof: false
+  bogof: false,
+  amount: 1
 }
 
 var secondItem = {
   name: "milk",
   price: 1.50,
-  bogof: false
+  bogof: false,
+  amount: 1
 }
 
 var thirdItem = {
   name: "cheese",
   price: 2.00,
-  bogof: true
+  bogof: true,
+  amount: 1
 }
 
 var fourthItem = {
   name: "lots of jelly",
   price: 25.50,
-  bogof: false
+  bogof: false,
+  amount: 1
 }
 
 module.exports = [firstItem, secondItem, thirdItem, fourthItem];
