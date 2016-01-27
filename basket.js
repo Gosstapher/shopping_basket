d9b68b0c35b2dc57ab87dce84867807f754dbf61
Gosstapher <gosstapher@gmail.com>
2016-01-27 08:10:29 +0000

    attempting a completely different method in another branch

--- basket.js
+++ basket.js
@@ -2,11 +2,20 @@ var items = require('./items')
 
 var basket = {
   totalBaseValue: 0,
+  totalReductionValue:0,
   totalFinalValue: 0,
   contents: [],
+  bogofArray: [],
   addItem: function(item){
     this.contents.push(item);
     this.totalBaseValue += item.price;
+  },
+  calculateBogofReduction: function(){
+    for(item in this.contents){
+      if(item.bogof === true){
+        this.bogofArray.push(item)
+      }
+    }
   },
   calculateTotalFinalValue: function(){
     if(this.totalBaseValue <= 19.99){
@@ -22,9 +31,13 @@ var basket = {
 // basket.addItem(items[0]);
 // basket.addItem(items[0]);
 // basket.addItem(items[1]);
-// basket.addItem(items[2]);
+basket.addItem(items[2]);
+basket.addItem(items[2]);
+basket.addItem(items[2]);
 // basket.addItem(items[3]);
-// console.log(basket.contents);
+basket.calculateBogofReduction();
+console.log(basket.contents);
+console.log(basket.bogofArray);
 // console.log(basket.totalBaseValue);
 // basket.calculateTotalFinalValue();
 // console.log(basket.totalFinalValue);
--- items.js
+++ items.js
@@ -1,21 +1,25 @@
 var firstItem = {
   name: "bread",
-  price: 0.50
+  price: 0.50,
+  bogof: false
 }
 
 var secondItem = {
   name: "milk",
-  price: 1.50
+  price: 1.50,
+  bogof: false
 }
 
 var thirdItem = {
   name: "cheese",
-  price: 2.00
+  price: 2.00,
+  bogof: true
 }
 
 var fourthItem = {
   name: "lots of jelly",
-  price: 25.50
+  price: 25.50,
+  bogof: false
 }
 
 module.exports = [firstItem, secondItem, thirdItem, fourthItem];
